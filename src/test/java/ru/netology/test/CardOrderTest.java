7e963725cb71c97baa8ac64a0e4ff8fb37c874df
Наталья Бачурина <natali.gorobets.86@mail.ru>
2022-08-23 20:34:13 +0700

    new tests

--- src/test/java/ru/netology/test/CardOrderTest.java
+++ src/test/java/ru/netology/test/CardOrderTest.java
@@ -1,7 +1,7 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -9,13 +9,14 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class CardOrderTest {
 
-    @BeforeAll
+    @BeforeEach
     void open() {
-        Selenide.open("http://localhost:9999/");
+        Selenide.open("http://localhost:9999");
     }
+
     @Test
     void completedForm() {
-        $("[data-test-id=name] input").setValue("Пётр Петров");
+        $("[data-test-id=name] input").setValue("Александр Иванов");
         $("[data-test-id=phone] input").setValue("+79509910505");
         $("[data-test-id=agreement]").click();
         $("button[type=button]").click();
